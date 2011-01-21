0b98b679cd03cb2edfd3e6fb66514d301f11d74e
David Pollak <feeder.of.the.bears@gmail.com>
2011-01-21 08:25:41 -0800

    Added some anti-script logic

--- src/main/scala/net/liftweb/seventhings/snippet/ChatIn.scala
+++ src/main/scala/net/liftweb/seventhings/snippet/ChatIn.scala
@@ -15,9 +15,15 @@ import js.JE._
  * to the ChatServer
  */
 object ChatIn {
+  // max count per session
+  private object lineCnt extends SessionVar(0)
+
   def render =
     "*" #> SHtml.onSubmit(s => {
-      ChatServer ! s // send the message
+      if (s.length < 50 && lineCnt < 20) { // 20 lines per session
+        ChatServer ! s // send the message
+        lineCnt.set(lineCnt.is + 1)
+      }
       SetValById("chat_in", "") // clear the input box
     })
 }
